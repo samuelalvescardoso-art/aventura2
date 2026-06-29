eeb4d2d43f257f20495cbc362d267008f6d8f0f3
samuelalvescardoso-art <samuel.alves.cardoso@escola.pr.gov.br>
2026-06-29 11:53:59 -0300

    2

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click, function()'{
     const atual = document.querySelector('.ativo');
-    const proximoPasso = ''
+    const proximoPasso = 'passo-' + this.getAtribbute('data-proximo');
+
+    atual.classlist.remove('ativo');
+    Document.getElementById(proximoPasso).classList.add('ativo');
  })
 
 })
